97c3d4c7cf16b11fe50c94d99b61b8bf18007016
Mohit Sharma <vipersharma.88@gmail.com>
2021-09-26 23:12:54 +0530

    Update

--- src/components/appbar.js
+++ src/components/appbar.js
@@ -4,7 +4,11 @@ export default class Appbar extends React.Component {
     render(){
         return (
             <div id="appbar">
-                <img src="./appbar.png"/>
+                <input id="appbar-search" type="search"></input>
+                <div id="appbar-space"></div>
+                <img id="appbar-icon" src="./message-icon.svg" />
+                <img id="appbar-icon" src="./notification-icon.svg" />
+                <h3>Dinesh Sharma</h3>
             </div>
         );
     }
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -6,36 +6,43 @@ export default class Sidebar extends React.Component {
         return (
             <div id="sidebar">
             <div id="sidebar-top">
-                <h2>CUnite</h2>
-                <img id="sidebar-icon" src="./menu.svg" />
+                <img id="sidebar-icon" src="./CUUnion-logo.svg" />
             </div>
             <div id="sidebar-mid">
                 <div className="sidebar-mid-option">
-                    <img className="sidebar-icon" src="./grid.svg" />
+                    <img className="sidebar-icon" src="./home-icon.svg" />
                     <NavLink to="/" exact="true" activeClassName="is-active">Dashboard</NavLink>
                 </div>
                 <div className="sidebar-mid-option">
-                    <img className="sidebar-icon" src="./home.svg" />
-                    <NavLink to="/explore-clubs" activeClassName="is-active" >Explore Clubs</NavLink>
+                    <img className="sidebar-icon" src="./projects-icon.svg" />
+                    <NavLink to="/explore-clubs" activeClassName="is-active" >Projects</NavLink>
                 </div>
                 <div className="sidebar-mid-option">
-                    <img className="sidebar-icon" src="./home.svg" />
-                    <NavLink to="/myclubs" activeClassName="is-active">My Clubs</NavLink>
+                    <img className="sidebar-icon" src="./inbox-icon.svg" />
+                    <NavLink to="/myclubs" activeClassName="is-active">Inbox</NavLink>
                 </div>
                 <div className="sidebar-mid-option">
-                    <img className="sidebar-icon" src="./home.svg" />
-                    <NavLink to="/mentorships" activeClassName="is-active">Mentorships</NavLink>
+                    <img className="sidebar-icon" src="./events-icon.svg" />
+                    <NavLink to="/mentorships" activeClassName="is-active">Events</NavLink>
                 </div>
                 <div className="sidebar-mid-option">
-                    <img className="sidebar-icon" src="./home.svg" />
-                    <NavLink to="/taskmanagement" activeClassName="is-active">Task Management</NavLink>
+                    <img className="sidebar-icon" src="./users-icon.svg" />
+                    <NavLink to="/taskmanagement" activeClassName="is-active">Clubs</NavLink>
                 </div>
                 <div className="sidebar-mid-option">
-                    <img className="sidebar-icon" src="./home.svg" />
-                    <NavLink to="/chatarea" activeClassName="is-active">Chat Area</NavLink>
+                    <img className="sidebar-icon" src="./events-icon.svg" />
+                    <NavLink to="/chatarea" activeClassName="is-active">Chat Room</NavLink>
                 </div>
                 <div className="sidebar-mid-option">
-                    <img className="sidebar-icon" src="./home.svg" />
+                    <img className="sidebar-icon" src="./calender-icon.svg" />
+                    <NavLink to="/settings" activeClassName="is-active">Calender</NavLink>
+                </div>
+                <div className="sidebar-mid-option">
+                    <img className="sidebar-icon" src="./help-icon.svg" />
+                    <NavLink to="/settings" activeClassName="is-active">Help Center</NavLink>
+                </div>
+                <div className="sidebar-mid-option">
+                    <img className="sidebar-icon" src="./setting-icon.svg" />
                     <NavLink to="/settings" activeClassName="is-active">Settings</NavLink>
                 </div>
             </div>
